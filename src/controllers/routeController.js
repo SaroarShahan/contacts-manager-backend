9e8e15aafe340f2a31e7cc9988e67a063c044eb7
SaroarShahan <saroar.shahan@gmail.com>
2023-02-15 02:49:09 +0600

    Add asyncHandler wrapper

--- src/controllers/routeController.js
+++ src/controllers/routeController.js
@@ -1,29 +1,68 @@
-exports.getAllContacts = async (req, res) => {
+const asyncHandler = require('../middleware/asyncHandler');
+const { createCustomError } = require('../utils/CustomError');
+
+/**
+ * @description Get all contacts
+ *
+ * @route GET /api/v1/contacts
+ * @access public
+ */
+exports.getContacts = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: 'Get all contacts',
   });
-};
+});
 
-exports.createContact = async (req, res) => {
+/**
+ * @description Create new contact
+ *
+ * @route POST /api/v1/contacts
+ * @access public
+ */
+exports.createContact = asyncHandler(async (req, res, next) => {
+  const { name, email, phone } = req.body;
+
+  if (!name || !email || !phone) {
+    return next(createCustomError('All the fields are required', 400));
+  }
+
   res.status(201).json({
     message: 'Contact created successfully',
   });
-};
+});
 
-exports.getContact = async (req, res) => {
+/**
+ * @description Get contact
+ *
+ * @route GET /api/v1/contacts/:id
+ * @access public
+ */
+exports.getContact = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: 'Get contact',
   });
-};
+});
 
-exports.updateContact = async (req, res) => {
+/**
+ * @description Update contact
+ *
+ * @route PUT /api/v1/contacts/:id
+ * @access public
+ */
+exports.updateContact = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: 'Contact updated successfully',
   });
-};
+});
 
-exports.deleteContact = async (req, res) => {
+/**
+ * @description Delete contact
+ *
+ * @route DELETE /api/v1/contacts/:id
+ * @access public
+ */
+exports.deleteContact = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: 'Contact deleted successfully',
   });
-};
+});
