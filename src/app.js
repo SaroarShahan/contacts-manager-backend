363b1dde6f01ef095ad0a600696930f2be579f36
SaroarShahan <saroar.shahan@gmail.com>
2023-02-15 00:27:42 +0600

    Update import path

--- src/app.js
+++ src/app.js
@@ -4,11 +4,8 @@ require('dotenv').config();
 const _PORT = process.env.PORT || 4000;
 const app = express();
 
-// Import All the routes here
-const contactRoutes = require('./routes/contactRoutes');
-
 // routes
-app.use('/api/v1/contacts', contactRoutes);
+app.use('/api/v1/contacts', require('./routes/contactRoutes'));
 
 app.listen(_PORT, () => {
   console.log(`Sever is running at http://localhost:${_PORT}`);
