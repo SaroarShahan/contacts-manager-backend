527da3d44c88b7cccc3565401349795248e70ec5
SaroarShahan <saroar.shahan@gmail.com>
2023-02-15 02:47:30 +0600

    Add Error handler

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,13 @@
+const { CustomError } = require('../utils/CustomError');
+
+exports.errorHandler = (err, req, res, next) => {
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).json({
+      message: err.message,
+    });
+  }
+
+  return res.status(500).json({
+    message: 'Something went wrong',
+  });
+};
