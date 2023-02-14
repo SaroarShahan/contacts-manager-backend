cac16f744a5e3273d41e5d474b1697985e12771d
SaroarShahan <saroar.shahan@gmail.com>
2023-02-15 00:38:22 +0600

    Add all the routes for contacts

--- src/routes/contactRoutes.js
+++ src/routes/contactRoutes.js
@@ -3,6 +3,12 @@ const router = express.Router();
 
 const contactsControllers = require('../controllers/routeController');
 
-router.route('/').get(contactsControllers.getAllContacts);
+router.route('/').get(contactsControllers.getAllContacts).post(contactsControllers.createContact);
+
+router
+  .route('/:id')
+  .get(contactsControllers.getContact)
+  .put(contactsControllers.updateContact)
+  .delete(contactsControllers.deleteContact);
 
 module.exports = router;
