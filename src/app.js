d4931eb3f66f08d81aad1597c0b28a0b95ef0a25
SaroarShahan <saroar.shahan@gmail.com>
2023-02-15 02:49:39 +0600

    Add middlewares

--- src/app.js
+++ src/app.js
@@ -4,8 +4,14 @@ require('dotenv').config();
 const _PORT = process.env.PORT || 4000;
 const app = express();
 
+const { errorHandler } = require('./middleware/errorHandler');
+
+// middlewares
+app.use(express.json());
+
 // routes
 app.use('/api/v1/contacts', require('./routes/contactRoutes'));
+app.use(errorHandler);
 
 app.listen(_PORT, () => {
   console.log(`Sever is running at http://localhost:${_PORT}`);
