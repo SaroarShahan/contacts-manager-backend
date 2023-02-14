6d337fceca73248ba466cce8009778984c54b0ab
SaroarShahan <saroar.shahan@gmail.com>
2023-02-15 00:38:02 +0600

    Create all the functions for controller

--- src/controllers/routeController.js
+++ src/controllers/routeController.js
@@ -3,3 +3,27 @@ exports.getAllContacts = async (req, res) => {
     message: 'Get all contacts',
   });
 };
+
+exports.createContact = async (req, res) => {
+  res.status(201).json({
+    message: 'Contact created successfully',
+  });
+};
+
+exports.getContact = async (req, res) => {
+  res.status(200).json({
+    message: 'Get contact',
+  });
+};
+
+exports.updateContact = async (req, res) => {
+  res.status(200).json({
+    message: 'Contact updated successfully',
+  });
+};
+
+exports.deleteContact = async (req, res) => {
+  res.status(200).json({
+    message: 'Contact deleted successfully',
+  });
+};
