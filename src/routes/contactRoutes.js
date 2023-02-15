667112d0988c5c58b209beeffa4b4b9d628a558b
SaroarShahan <saroar.shahan@gmail.com>
2023-02-16 00:54:50 +0600

    Add validate token middleware

--- src/routes/contactRoutes.js
+++ src/routes/contactRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const contactsControllers = require('../controllers/routeController');
+const contactsControllers = require('../controllers/contactController');
+const validateToken = require('../middleware/validateTokenHandler');
 
+router.use(validateToken);
 router.route('/').get(contactsControllers.getContacts).post(contactsControllers.createContact);
-
 router
   .route('/:id')
   .get(contactsControllers.getContact)
